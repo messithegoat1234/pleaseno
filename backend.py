a80bcb0c50438ab38765f25e7c081a8ea491396c
messithegoat1234 <leonoszajca@gmail.com>
2026-08-27 20:28:06 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -17,11 +17,12 @@ import os
 
 def get_db():
     return mysql.connector.connect(
-        host=os.environ["DB_HOST"],
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"],
-        database=os.environ["DB_NAME"],
-        port=int(os.environ.get("DB_PORT", 27565))
+        host="mysql-2a46d1fc-pleaseno.l.aivencloud.com",
+        port=27565,
+        user="avnadmin",
+        password="",
+        database="pleasenoinfo",
+        ssl_ca="ca.pem"
     )
 
 @app.route("/products")
